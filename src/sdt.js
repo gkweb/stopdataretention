a00a0eb14f3d2d10b38ef41c7ce92bb4a0aefe94
gkweb <gladekettle@gmail.com>
2014-10-06 13:04:09 +1100

    mobile padding fix

--- src/sdt.js
+++ src/sdt.js
@@ -1,28 +1,22 @@
+"use strict";
 // Glade Kettle of gladekettle.com.au, public domain. Forka forka, please! http://github.com/gkweb/
-// var now = new Date();
-function toggleClass(element, className){
-	console.log(element);
-    if (!element || !className){
+function toggleClass (element, className) {
+    if (!element || !className) {
         return;
     }
     var classString = element.className, nameIndex = classString.indexOf(className);
     if (nameIndex == -1) {
         classString += ' ' + className;
-    }
-    else {
+    } else {
         classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
     }
     element.className = classString;
 }
 
-// if (now.getDate() == 18 && now.getMonth() == 0 && now.getFullYear() == 2014) {
-
-    window.onload = function(){
+window.onload = function (){
       var oBody = document.getElementsByTagName('body')[0]; 
-      // oBody.style.cssText = "background: #000000; text-align: center; color: #111; font: 2.2em Helvetica; vertical-align: middle"; 
       oBody.innerHTML += '<div class="sdt-main" id="sdt-main"> <div class="sdt-inner"> <div class="sdt-cnr"> <img src="http://rawgit.com/gkweb/stopdataretention/master/src/images/stretch.png" class="stretch"> <a href="#" id="sdtopen"> <img id="sdtfacebanner" src="http://rawgit.com/gkweb/stopdataretention/master/src/images/sdt-corner@2x.png" alt="Stop Data retention!"> <img id="sdtfacebanner2" class="animated animatedmm infinite mouth" src="http://rawgit.com/gkweb/stopdataretention/master/src/images/sdt-corner-2@2x.png" alt="Stop Data retention!"> </a> <div class="sdtcnrtxt-container animated flash infinite"> <img id="sdtcnrtxt" src="http://rawgit.com/gkweb/stopdataretention/master/src/images/stop_data_retention_txt@2x.png" alt=""></div> </div> </div> </div> <div class="sdt-modal"> <div class="sdt-inner"> <div class="sdt-container"> <a href="#" class="sdt-close-btn" id="sdt-modal-close-btn">×</a> <div class="sdt-content"> <h1>The government wants easy access to your phone location, who you&rsquo;ve been calling and what you&rsquo;ve been searching on google. Additionally they wanto store all this private data for 2 years!</h1> <p>This data isn&rsquo;t necassarily needed to combat terrorism. This data may be accessed by other government agencies, or even third party organizations.</p> <p>Without your help this <strong>will</strong> affect the future of Australia.</p> <a target="_blank" href="http://stopdataretention.info">We need to STOP THIS from happening NOW!</a> </div> </div> </div> </div>'; 
     
-      var sdtM = document.getElementById("sdt-main");
       var sdtO = document.getElementById("sdtopen");
       var sdtB = document.getElementById("sdt-modal-close-btn");
 
@@ -33,7 +27,4 @@ function toggleClass(element, className){
       sdtB.onclick = function () {
         toggleClass(oBody,"topen");
       };
-
-
-    }
-// } if
+};
